18343fc7ca382bd301e8ce33efb33a727a63e4e6
Patrick Baselier <patrick.baselier@gmail.com>
2016-01-15 16:23:24 +0100

    Extract link_to

--- helpers/author_helpers.rb
+++ helpers/author_helpers.rb
@@ -1,7 +1,9 @@
 module AuthorHelpers
 
-  def link_to_profile(text, author)
-    link_to text, "profiles/#{author.nameAsSlug}"
+  # @example:
+  #   link_to_profile(author) { author.name }
+  #   #=> <a href="profiles/eddie-van-halen">Eddie van Halen</a>
+  def link_to_profile(author, &block)
+    link_to yield, "profiles/#{author.nameAsSlug}", class: 'profile-link'
   end
 end
-
