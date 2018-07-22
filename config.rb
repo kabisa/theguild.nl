2c9412128907c30291562abdf3703043ef20c969
Eric Bartholemy <eric@kabisa.nl>
2018-07-22 11:49:58 +0200

    disable http_prefix

--- config.rb
+++ config.rb
@@ -92,7 +92,7 @@ configure :build do
   # activate :relative_assets
 
   # Or use a different image path
-  set :http_prefix, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
 end
 
 activate :directory_indexes
