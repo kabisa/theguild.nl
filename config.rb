e013fa8e68b6fa1cbb94b0cb958d7768275a68b1
Eric Bartholemy <eric@kabisa.nl>
2018-07-22 11:10:12 +0200

    set after_configuration

--- config.rb
+++ config.rb
@@ -108,12 +108,14 @@ activate :syntax, line_numbers: true
 # Activate before using any ENV defined in `.env`
 activate :dotenv
 
-# set :posts, @app.data.site.post.values.sort_by(&:created_on).reverse
+after_configuration do
+  set :posts, @app.data.site.post.values.sort_by(&:created_on).reverse
 
-# posts.each do |post|
-  # proxy "#{post.slug}.html",
-        # 'templates/post.html', locals: { post: post }, ignore: true
-# end
+  posts.each do |post|
+    proxy "#{post.slug}.html",
+      'templates/post.html', locals: { post: post }, ignore: true
+  end
+end
 
 # Create RSS Feed xml
 page @app.data.config.feed_path, layout: false
