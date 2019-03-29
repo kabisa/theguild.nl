8602ea3c10624707342cf1b846425a7eb9466bdb
Patrick Baselier <patrick.baselier@gmail.com>
2019-03-29 17:23:27 +0100

    Extract

--- helpers/navbar_helpers.rb
+++ helpers/navbar_helpers.rb
@@ -2,4 +2,11 @@ module NavbarHelpers
   def navbar_items
     data.config.navbar
   end
+
+  def navbar_item_class_name(request_path, item)
+    # This does not work properly in dev mode
+    # but in a built project
+    # it looks as expected.
+    request_path == "#{item.url}/index.html".gsub('//', '/') ? 'active' : nil
+  end
 end
