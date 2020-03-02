7031a408c76bcba862993590977c2aa6afbdb020
Eric Bartholemy <eric@kabisa.nl>
2020-03-02 18:26:34 +0100

    move the companion for the service  worker to the footer of the html

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,14 +1,3 @@
 //= require ./_console-jobs
 //= require ./delLocalStorage
 
-if (navigator.serviceWorker) {
-  navigator.serviceWorker
-    .register("/javascripts/serviceworker.js", { scope: "./" })
-    .then(function() {
-      console.log("[Companion]", "Service worker registered!")
-    })
-    .catch(function(error) {
-      // registration failed :(
-      console.log("[Companion]", "Service worker registration failed: " + error)
-    })
-}
