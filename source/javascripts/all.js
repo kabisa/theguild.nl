7506df7106f81031e72c95365230d31e483fbb2e
Eric Bartholemy <eric@kabisa.nl>
2020-03-02 18:16:01 +0100

    instead of require_tree, using require

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,14 @@
-//= require_tree .
+//= require ./_console-jobs
+//= require ./delLocalStorage
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register("/javascripts/serviceworker.js", { scope: "./" })
+    .then(function() {
+      console.log("[Companion]", "Service worker registered!")
+    })
+    .catch(function(error) {
+      // registration failed :(
+      console.log("[Companion]", "Service worker registration failed: " + error)
+    })
+}
